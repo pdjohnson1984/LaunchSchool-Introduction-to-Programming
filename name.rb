6037e664544fc7e86c553d4f8c2011da1f60ab78
Phillip <pdjohnson84@gmail.com>
2016-07-26 18:46:38 -0700

    Adding exercises for Variables lesson

---
+++ name.rb
@@ -0,0 +1,13 @@
+puts "Please enter your name:"
+name = gets.chomp
+puts "Hello #{name}!!!"
+
+10.times do 
+  puts name
+end
+
+puts "Please enter your first name:"
+first_name = gets.chomp
+puts "Please enter your last name:"
+last_name = gets.chomp
+puts "Your full name is #{first_name} #{last_name}!!!"
